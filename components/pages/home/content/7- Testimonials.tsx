342e0b3ef47bac75e25a08e8940107179fcc75d4
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2022-10-20 01:10:33 +0100

    🔥Added Testimonials Section

--- components/pages/home/content/7- Testimonials.tsx
+++ components/pages/home/content/7- Testimonials.tsx
@@ -1,4 +1,46 @@
 import React from 'react'
+import Rating from '@mui/material/Rating';
+import Image from 'next/image'
+
+
+
+function Comment(props: any) {
+    return (
+        <>
+            <div className="Comment">
+                <div className="Stars">
+                    <Rating name="read-only" value={4} readOnly />
+                </div>
+
+                <div className="Text">
+                    Sit adipiscing feugiat aliquam elementum tellus iaculis urna, purus pulvinar. Dolor habitasse vitae nulla ut ultrices nulla venenatis, condimentum. In augue sapien sed nascetur tellus. Nec donec eu luctus fermentum pellentesque viverra venenatis, mauris. Sit lobortis gravida vitae ipsum viverra feugiat. Sapien senectus orci.
+                </div>
+
+                <div className="Person">
+                    <div className="Picture">
+                        <Image src="/img/assets/BusinessManUsingLaptop.png" layout="fill" objectFit="cover"/>
+                    </div>
+
+                    <div className="Name">
+                        <div className="FullName">Yassine Chettouch</div>
+                        <div className="Profession">Developer</div>
+                    </div>
+                </div>
+                
+
+            </div>
+        </>
+    )
+}
+
+
+function CommentDisplay(props: any) {
+    return(
+        <>
+        
+        </>
+    )
+}
 
 
 export default function Testimonials() {
@@ -15,7 +57,19 @@ export default function Testimonials() {
                         Proin id pellentesque laoreet pretium sem pharetra. 
                     </p>
                 </div>
+
+                <div className="CommentsDisplay">
+                    <Comment/>
+                    <Comment/>
+                </div>
+
+                <div className="Summary">
+                    <div className="Index Active"></div>
+                    <div className="Index"></div>
+                    <div className="Index"></div>
+                </div>
             </div>
+
         </>
     )
 }
