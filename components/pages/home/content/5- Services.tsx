474e12ff60b41f27b75e52b6a406e103d126bfbb
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2022-10-23 14:36:45 +0100

    This shit drives me crazy dude

--- components/pages/home/content/5- Services.tsx
+++ components/pages/home/content/5- Services.tsx
@@ -1,15 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
 
-// Icon of each Service
-import Coding from "/Public/img/assets/3D-icons/Google Code.png"
-import Design from "/Public/img/assets/3D-icons/Design.png"
-import Advertising from "/Public/img/assets/3D-icons/Commercial.png"
-import EMarketing from "/Public/img/assets/3D-icons/Goal.png"
-import Business from "/Public/img/assets/3D-icons/Business.png"
-import Visual from "/Public/img/assets/3D-icons/Xlarge Icons.png"
-
-
 
 const Card = (props: any) => {
     return (
@@ -39,36 +30,36 @@ const Card = (props: any) => {
 
 export default function Services() {
 
-    const Cards: {title: string, description: string, icon: StaticImageData}[] = [
+    const Cards: {title: string, description: string, icon: string}[] = [
         {
             title: "Design and programming of websites and applications",
             description: "This is with the highest quality in terms of design, coding and various aspects of the site, which provides our customers with modification in the future, as we take into account all the options that the customer may need so that he does not have to design the site again, which may cost money and time",
-            icon: Coding
+            icon: "/Public/img/assets/3D-icons/Google Code.png"
         },
         {
             title: "Graphic design and print",
             description: "Design is part of our core personality. We develop new ideas from scratch based on available materials and information provided. We also provide customized solutions to reach our marketing goals. Through design we create the optimal communication picture that will work perfectly for the benefit of your business.",
-            icon: Design
+            icon: "/Public/img/assets/3D-icons/Design.png"
         },
         {
             title: "Advertising campaigns",
             description: "Advertising campaigns improve the reach of a company's website or social media accounts, but they require a lot of experience and good planning to ensure good results.",
-            icon: Advertising
+            icon: "/Public/img/assets/3D-icons/Commercial.png"
         },
         {
             title: "E-Marketing",
             description: "We market events and events through all websites and electronic platforms and ensure that the public is informed and attended",
-            icon: EMarketing
+            icon: "/Public/img/assets/3D-icons/Goal.png"
         },
         {
             title: "Building a business identity",
             description: "The brand identity has become the biggest strategy that generates millions of dollars for entrepreneurs, so it was necessary for the Media Man team to rely on all the foundations in their work to build a harmonious brand identity for clients",
-            icon: Business
+            icon: "/Public/img/assets/3D-icons/Business.png"
         },
         {
             title: "visual production",
             description: "We have a creative team in the fields of writing, directing and editing to provide you with high services, using the latest technical technologies and innovative ideas to produce works that can remain in the public's memory for as long as possible.",
-            icon: Visual
+            icon: "/Public/img/assets/3D-icons/Xlarge Icons.png"
         }
     ]
 
