47a0b9ce882eaa11540ccd48fb430893bebffed1
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2022-11-13 16:12:21 +0100

    Fixed Empty Pages Error

---
+++ components/pages/services/marketing/page.tsx
@@ -0,0 +1,17 @@
+import Header from "../../../layout/header";
+import Footer from "../../../layout/footer";
+
+export default function Page() {
+
+
+    return (
+        <div className ="DesignPage">
+            <Header/>
+
+            <div className="PageContent">
+            </div>
+
+            <Footer/>
+        </div>
+    )
+}
