e7197f16ab831238a83140115398ffc3504d7605
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2022-11-28 18:36:08 +0100

    ✅All Services got their page - Task Complete

--- components/pages/services/marketing/page.tsx
+++ components/pages/services/marketing/page.tsx
@@ -4,6 +4,9 @@ import Card from "./imports/1- Card";
 import Slogan from "../global/Slogan";
 import StandOut from "./imports/3 - Stand out";
 import BestServices from "./imports/4 - Best Services";
+import WhyUs from "./imports/5- Why Us";
+import LetsTalk from "../../home/content/8- Let's Talk";
+import FAQ from "./../global/FAQ";
 
 export default function Page() {
     return (
@@ -15,6 +18,9 @@ export default function Page() {
                 <Slogan text="Marketing has always been about relationships. About trust and personalised service. And about human connection. In a world that feels unpredictable and uncertain, and where you have more conversations with AI than with people, Orenzo is a shift towards human-driven marketing." />
                 <StandOut />
                 <BestServices />
+                <WhyUs />
+                <LetsTalk />
+                <FAQ />
             </div>
 
             <Footer />
