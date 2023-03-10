e91d8f2081a20624f9e3b55095331c07a7e639a8
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2023-03-10 23:20:58 +0100

    ✅Merge Log Cleared

--- components/pages/services/development/imports/TechnologyStack.tsx
+++ components/pages/services/development/imports/TechnologyStack.tsx
@@ -1,77 +1,89 @@
 import { useState, useRef, useEffect } from "react";
 import { IconButton } from "@mui/material";
 import { DefaultButton } from "../../../../core/buttons";
-import CloseIcon from '@mui/icons-material/Close';
-import Chip from '@mui/material/Chip';
+import CloseIcon from "@mui/icons-material/Close";
+import Chip from "@mui/material/Chip";
 import { ProgrammingRange } from "./data/programmingRange";
-import { useTranslation } from 'next-i18next';
+import { useTranslation } from "next-i18next";
 
 const Language = (props: any) => {
     return (
         <>
-            <Chip className="Chip" label={props.Name} icon={<props.Logo/>} clickable/>
+            <Chip
+                className="Chip"
+                label={props.Name}
+                icon={<props.Logo />}
+                clickable
+            />
         </>
-    )
-}
+    );
+};
 
 export default function TechnologyStack() {
     const [isOpen, setIsOpen] = useState(false);
-
-<<<<<<< HEAD
-    const title = t('services.development.content.fourth.title');
-    const description = t('services.development.content.fourth.description');
-=======
-    const { t } = useTranslation('common');
+    const { t } = useTranslation("common");
     console.log(t);
->>>>>>> 04079481a69614ac5b4c89f1ab7758f28bbb03a9
+
+    const title = t("services.development.content.fourth.title");
+    const description = t("services.development.content.fourth.description");
 
     return (
         <>
             <div className="TechnologyStack">
-                    <div className="Column">
-                    </div>
-                    <div className="Column">
-                        <div className="Text">
-                            <div className="Tilte">
-                                {t('services.development.content.fourth.title')}
-                            </div>
-                            <div className="Description">
-                                {t('services.development.content.fourth.description')}
-                            </div>
+                <div className="Column"></div>
+                <div className="Column">
+                    <div className="Text">
+                        <div className="Tilte">
+                            {t("services.development.content.fourth.title")}
                         </div>
-                        <DefaultButton bgColor="White" onClick={() => {setIsOpen(!isOpen)}}>
-                            {t('services.development.content.fourth.button')}
-                        </DefaultButton>
+                        <div className="Description">
+                            {t(
+                                "services.development.content.fourth.description"
+                            )}
+                        </div>
                     </div>
+                    <DefaultButton
+                        bgColor="White"
+                        onClick={() => {
+                            setIsOpen(!isOpen);
+                        }}
+                    >
+                        {t("services.development.content.fourth.button")}
+                    </DefaultButton>
+                </div>
             </div>
 
-            {
-                isOpen ? (
-                    <div className="Technologies">
-                        <div className="Container">
-                            <div className="Head">
-                                <IconButton className="CloseBtn" aria-label="Close" onClick={() => {setIsOpen(!isOpen)}}>
-                                    <CloseIcon fontSize="inherit"/>
-                                </IconButton>
-                            </div>
+            {isOpen ? (
+                <div className="Technologies">
+                    <div className="Container">
+                        <div className="Head">
+                            <IconButton
+                                className="CloseBtn"
+                                aria-label="Close"
+                                onClick={() => {
+                                    setIsOpen(!isOpen);
+                                }}
+                            >
+                                <CloseIcon fontSize="inherit" />
+                            </IconButton>
+                        </div>
 
-                            <div className="Body">
-                                {
-                                    ProgrammingRange.map((Lang, index) => {
-                                        return (
-                                            <Language key={index} Name={Lang.name} Logo={Lang.logo}/>
-                                        )
-                                    })
-                                }
-                            </div>
+                        <div className="Body">
+                            {ProgrammingRange.map((Lang, index) => {
+                                return (
+                                    <Language
+                                        key={index}
+                                        Name={Lang.name}
+                                        Logo={Lang.logo}
+                                    />
+                                );
+                            })}
                         </div>
                     </div>
-                ) : (
-                    <>
-
-                    </>
-                )
-            }
+                </div>
+            ) : (
+                <></>
+            )}
         </>
-    )
+    );
 }
