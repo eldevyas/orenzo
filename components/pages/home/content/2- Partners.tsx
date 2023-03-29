215a148bdf2081d794ea758aafa4d856abfc1c61
Yassine Chettouch <95131504+yassine-ct@users.noreply.github.com>
2023-03-29 03:01:11 +0000

    ⚡Just pushed before going to sleep.

--- components/pages/home/content/2- Partners.tsx
+++ components/pages/home/content/2- Partners.tsx
@@ -1,14 +1,37 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { Trans, useTranslation } from "react-i18next";
-
-const Companies = [
-    "/img/assets/partners/RoyalAir.png"
-];
+import Slider from "react-slick";
 
 export default function TrustedPartners() {
+    var Companies: any[] = [];
+    //
+    //
+    Companies[0] = "/img/assets/partners/First.png";
+    Companies[1] = "/img/assets/partners/Second.png";
+    Companies[2] = "/img/assets/partners/Third.png";
+    Companies[3] = "/img/assets/partners/Fourth.png";
+    Companies[4] = "/img/assets/partners/Fifth.png";
+    Companies[5] = "/img/assets/partners/Sixth.png";
+    Companies[6] = "/img/assets/partners/Seventh.png";
+    Companies[7] = "/img/assets/partners/Eighth.png";
+    Companies[8] = "/img/assets/partners/Ninth.png";
+    Companies[9] = "/img/assets/partners/Tenth.png";
+    //
+    //
     const { t } = useTranslation("common");
-
+    const Options = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        ease: "linear",
+        centerMode: true,
+    };
     return (
         <>
             <div className="TrustedPartners" id="Partners">
@@ -16,32 +39,32 @@ export default function TrustedPartners() {
                     {t("home.content.TrustedPartners.title")}
                 </div>
 
-                <section className="Partners" dir="ltr">
-                    <div className="Wrapper">
-                        {Companies.map((Partner, index) => {
-                            return (
-                                <div key={index} className="Slide">
-                                    <Image
-                                        alt=""
-                                        src={Partner}
-                                        layout="fill"
-                                        objectFit="contain"
-                                    />
-                                </div>
-                            );
-                        })}
-                        {Companies.map((Partner, index) => {
-                            return (
-                                <div key={index} className="Slide">
-                                    <Image
-                                        alt=""
-                                        src={Partner}
-                                        layout="fill"
-                                        objectFit="contain"
-                                    />
-                                </div>
-                            );
-                        })}
+                <section className="TrustedPartners__Partners" dir="ltr">
+                    <div className="TrustedPartners__Partners__Wrapper">
+                        <Slider
+                            {...Options}
+                            className="TrustedPartners__Partners__Wrapper__Slider"
+                        >
+                            {Companies.map((Company, Index) => {
+                                return (
+                                    <>
+                                        <div
+                                            key={Index}
+                                            className="TrustedPartners__Partners__Wrapper__Slider__Slide"
+                                        >
+                                            <div className="TrustedPartners__Partners__Wrapper__Slider__Slide__Logo">
+                                                <Image
+                                                    src={Company}
+                                                    alt=""
+                                                    layout="fill"
+                                                    objectFit="contain"
+                                                />
+                                            </div>
+                                        </div>
+                                    </>
+                                );
+                            })}
+                        </Slider>
                     </div>
                 </section>
             </div>
